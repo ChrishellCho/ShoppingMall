3a1aae83258e516ff417ae6c663f875c383c75b0
YeJin Cho <83330070+gkdldu@users.noreply.github.com>
2021-06-02 10:13:59 +0900

    SQL: Register users procedure

--- Procedure.sql
+++ Procedure.sql
@@ -1,10 +1,26 @@
-CREATE PROCEDURE usp_SigninUsers
+USE ShopDB;
+
+-- 회원가입 저장 프로시저
+CREATE PROCEDURE usp_AddUsers
 	@UserID NVARCHAR(25),
 	@Password NVARCHAR(20),
 	@UserName nvarchar(10),
 	@Introducer NVARCHAR(25),
 	@Address nvarchar(50)
 AS
-	INSERT INTO dbo.Users VALUES (@UserID, @Password, @UserName, @Introducer);
+	DECLARE @Result INT
+	SELECT @Result= COUNT(*) FROM Users
+	WHERE UserId=@UserID
+
+	IF @Result >0
+	RETURN -1
+	ELSE
+	BEGIN
+		INSERT Users (UserID, Password, UserName, Introducer, Address)
+		VALUES (@UserID, @Password, @UserName, @Introducer, @Address)
+		RETURN @@Identity
+	END
+GO
+
 
 
