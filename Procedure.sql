5ab648e15e024da797a096f97cbd1b728106474f
YeJin Cho <83330070+gkdldu@users.noreply.github.com>
2021-06-10 15:25:14 +0900

    Add postcode

--- Procedure.sql
+++ Procedure.sql
@@ -1,11 +1,12 @@
 USE ShopDB;
 
 -- 1.회원가입 저장 프로시저
-CREATE PROCEDURE usp_AddUsers
+CREATE PROC usp_AddUser
 	@UserID NVARCHAR(25),
 	@Password NVARCHAR(20),
 	@UserName nvarchar(10),
 	@Introducer NVARCHAR(25),
+	@Postcode int,
 	@Address nvarchar(50)
 AS
 	-- 이미 가입된 아이디라면
@@ -17,8 +18,8 @@ AS
 		RETURN -1 -- -1이 반환되면 회원가입 불가
 	ELSE
 	BEGIN
-		INSERT Users (UserID, Password, UserName, Introducer, Address)
-		VALUES (@UserID, @Password, @UserName, @Introducer, @Address)
+		INSERT Users (UserID, Password, UserName, Introducer,Postcode, Address)
+		VALUES (@UserID, @Password, @UserName, @Introducer,@Postcode, @Address)
 		RETURN @@Identity
 	END
 GO
@@ -28,6 +29,7 @@ CREATE PROC usp_LoginUser
 	@UserID	VARCHAR(20),
 	@Password VARCHAR(20)
 AS
+	-- ID와 암호가 일치하는지
 	DECLARE @Result INT
 	SELECT @Result=COUNT(*) FROM Users
 	WHERE UserID=@UserID AND Password=@Password
@@ -41,4 +43,12 @@ AS
 		SELECT '-1' -- -1이 반환되면 로그인 불가
 	END
 GO
+
+CREATE PROC usp_GetUsers
+AS
+	SELECT*FROM Users ORDER BY UID DESC
+GO
+
+DROP PROC usp_AddUser;
+
 
