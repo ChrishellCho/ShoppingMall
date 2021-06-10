d0cb7154c3dde85f382580e0f718acc78fe72ccb
YeJin Cho <83330070+gkdldu@users.noreply.github.com>
2021-06-10 15:31:55 +0900

    Modify product category table

--- UsersTable.sql
+++ UsersTable.sql
@@ -13,27 +13,34 @@ CREATE TABLE dbo.Users(
  )
 
 INSERT INTO Users VALUES ('jin','1234','조예진','tae',16690,'경기도 수원시');
-SELECT * FROM Users;
+SELECT * FROM Users;	
 
 DROP TABLE DBO.Users;
 
 
 --카테고리 테이블
-CREATE TABLE goods_category(
+CREATE TABLE Pro_Category(
 	cateName nvarchar(20) not null,
 	cateCode varchar(30) not null,
 	cateCodeRef varchar(30),
 	primary key(cateCode),
-	foreign key (cateCodeRef) references goods_category (cateCode)
+	foreign key (cateCodeRef) references Pro_Category (cateCode)
 );
 
-INSERT INTO goods_category (cateName, cateCode) values ('카테고리1', '100');
-insert into goods_category values ('카테고리1-1','101','100');
+INSERT INTO Pro_Category (cateName, cateCode) values ('카테고리1', '100');
+insert into Pro_Category values ('카테고리1-1','101','100');
 
-INSERT INTO goods_category (cateName, cateCode) values ('카테고리2', '200');
-insert into goods_category values ('카테고리2-1','202','200');
-insert into goods_category values ('카테고리2-2','203','200');
+INSERT INTO Pro_Category (cateName, cateCode) values ('카테고리2', '200');
+insert into Pro_Category values ('카테고리2-1','202','200');
+insert into Pro_Category values ('카테고리2-2','203','200');
 
-SELECT * FROM goods_category;
+SELECT * FROM Pro_Category;
 
-DROP TABLE goods_category;
+DROP TABLE Pro_Category;
+
+
+
+--상품 테이블
+CREATE TABLE Products
+(
+	
